0cdfa3c9353cd0fc27ac59e5c3694997492252e9
Mohammed Mirsa <mohammedmirsa010@gmail.com>
2022-04-28 12:18:24 +0530

    CustomPaint changed to custom clipper

--- lib/UI/LoginScreen/LoginScreen.dart
+++ lib/UI/LoginScreen/LoginScreen.dart
@@ -1,15 +1,43 @@
-import 'package:ecommerceapi/UI/Designs/ContainerBlue.dart';
 import 'package:flutter/material.dart';
+import '../../CustomDesigns/CustomShapeLogin.dart';
 
 class ScreenLogin extends StatelessWidget {
   const ScreenLogin({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: CustomPaint(
-        foregroundPainter: ContainerBlue(),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                colors: [Color(0x7D7D65EC), Colors.blue],
+                begin: FractionalOffset(0.5, 0.0),
+                end: FractionalOffset(0.0, 0.5),
+              )),
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: ClipPath(
+                  clipper: Customshape(),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * .70,
+                    width: MediaQuery.of(context).size.width,
+                    color: Colors.white,
+                    child: Center(
+                        child: Text(
+                      "GeeksforGeeks",
+                      style: TextStyle(fontSize: 40, color: Colors.white),
+                    )),
+                  )),
+            )
+          ],
+        ),
       ),
     );
   }
